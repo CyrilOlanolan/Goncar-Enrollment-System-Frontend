b9b0a8bb02036a0973fc0cd59ec765b3154d035c
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-07-25 13:18:56 +0800

    Installed AG Grid library
        > AG Grid Library: for data grid/table
        > Added sample Trainee data
        > Trainee table, WIP

--- client/src/components/Pages/Trainees/Trainees.jsx
+++ client/src/components/Pages/Trainees/Trainees.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
 
 import {
   SideBar,
@@ -6,7 +10,61 @@ import {
 } from '../../ComponentIndex'
 import styles from './Trainees.module.scss';
 
+import sampleTrainees from '../../sampleData/sampleTrainees.json';
+
 const Trainees = () => {
+  /* SET COLUMN DEFINITIONS */
+  const [columnDefs] = useState([
+    {
+      field: "traineeID",
+      headerName: "ID",
+      lockPosition: "left",
+      width: 50
+    },
+    {
+      field: "id",
+      headerName: "Full Name",
+      lockPosition: "left",
+      cellRenderer: getFullName,
+      width: 233
+    },
+    {
+      field: "",
+      headerName: "Current Course",
+      lockPosition: "left"
+    },
+    {
+      field: "",
+      headerName: "Status",
+      lockPosition: "left"
+    },
+    {
+      field: "",
+      headerName: "Action",
+      lockPosition: "left"
+    },
+  ])
+
+  /* INITIALIZE rowData VARIABLE */
+  const [rowData, setRowData] = useState([]);
+
+  /* FETCH DATA ON COMPONENT MOUNT */
+  /* TODO: Implement API fetching here */
+  useEffect(
+    () => {
+      setRowData(sampleTrainees);
+    }
+  , [rowData])
+
+  console.log(rowData)
+
+  function getFullName(params) {
+    return `${params.data.lastName}, ${params.data.firstName} ${getMiddleInitial(params)}.`
+  }
+
+  function getMiddleInitial(params) {
+    return params.data.middleName[0];
+  }
 
   return (
     <>
@@ -14,9 +72,7 @@ const Trainees = () => {
       <div className={styles["Trainees"]}>
         <BubblePage>
           <h1>Trainee Masterlist</h1>
-          <div className={styles["Trainees__table"]}>
-
-          </div>
+          <AgGridReact columnDefs={columnDefs} rowData={rowData} />
         </BubblePage>
       </div>
     </>
