8359b06d605557f1fdf65d6c3263365017912b9a
yohansolab <60298480+yohansolab@users.noreply.github.com>
2022-09-02 00:00:07 +0800

    Administartive page

--- client/src/components/Pages/Administrative/Administrative.jsx
+++ client/src/components/Pages/Administrative/Administrative.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import {SideBar} from '../../ComponentIndex';
 
 const Administrative = () => {
   return (
---
+++ client/src/stories/Pages/Administrative/Aministrative.stories.js
@@ -0,0 +1,10 @@
+import { Administrative } from "../../../components/ComponentIndex";
+
+export default{
+    title: 'Administrative Page',
+    component: Administrative
+};
+
+export const Main = {
+    args: {}
+};
