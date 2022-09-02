204bbc2f0ae1b5a36c1c09c65864cbc8b9d635db
yohansolab <60298480+yohansolab@users.noreply.github.com>
2022-09-03 01:37:16 +0800

    .

--- client/src/components/Pages/Administrative/Administrative.jsx
+++ client/src/components/Pages/Administrative/Administrative.jsx
@@ -1,10 +1,88 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import { AgGridReact } from 'ag-grid-react'; //AG grid react component
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
 
-import {SideBar} from '../../ComponentIndex';
+import {SideBar, BubblePage, WelcomeBubble, DashboardCards, 
+  BreadcrumbsComponent} from '../../ComponentIndex';
+import styles from "./Administrative.module.scss"
 
 const Administrative = () => {
+
+  var breadcrumbsRoutes = [
+    {
+      label: "Dashboard",
+      href: '/dashboard',
+      onclick: () => console.log("Hi")
+    },
+    {
+      label: "Administrative",
+      href: "/Administrative",
+    }
+  ];
+
+   //TABLE
+   const [rowData, setRowData] = useState([]);
+   const [columnDefs] = useState ([
+     {
+       field: "courseName",
+       headerName: "Course Name",
+       lockPosition: "left",
+       width: 230,
+       sortable: true
+     },
+     {
+       field: "yearSpan",
+       headerName: "Year Span",
+       lockPosition: "left",
+       width: "150",
+       sortable: true
+     },
+     {
+       field: "courseUnits",
+       headerName: "Units",
+       lockPosition: "left",
+       width:"75",
+       sortable: true,
+     },
+     {
+       field:"courseTuition",
+       headerName:"Tuition",
+       lockPosition:"left",
+       width:"150",
+       sortable: true
+     }
+   ])
+ 
+ 
+ 
+   const gridOptions = {
+     defaultColDef: {
+       filter: true
+     },
+     columnDefs: columnDefs,
+     rowData: rowData,
+     pagination: true,
+     paginationAutoPageSize: true
+   }
+
+ 
+
   return (
-    <div>Administrative</div>
+    <>
+      <SideBar />
+      <div classname="container">
+        <BubblePage>
+          <BreadcrumbsComponent routes={breadcrumbsRoutes} />
+          <h3>Courses</h3>
+
+          
+
+
+        </BubblePage>
+
+
+      </div>
+    </>
   )
 }
 
