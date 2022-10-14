afabbbfb06b3c00aa870b91fa5bb2c3ce734fd94
yohansolab <60298480+yohansolab@users.noreply.github.com>
2022-10-14 14:36:53 +0800

    Added: donwloadJS, axios utility, small trainee profile update

---
+++ client/src/assets/utilities/Print.js
@@ -0,0 +1,31 @@
+import donwload from 'downloadjs';
+import axios from  'axios';
+
+let deployedURI = 'https://goncar-system-backend.herokuapp.com';
+
+export function getTraineeProfile(id) {
+    return axios
+    .get(`${deployedURI}/api/trainees/${id}`, {
+        headers: this.headers,
+        responseType: 'blob',
+    })
+    .then(function (response) {
+        return response.status;
+    })
+    .catch(function (error){
+        return error.rsponse.status;
+    })
+}
+
+export function downloadFile(pdf); {
+    axios
+        .get(`${deployedURI}/api/trainees/${id}`, {
+            headers: this.headers,
+            responseType: 'blob', // had to add this one here
+        })
+        .then(response => {
+           const content = response.headers['content-type'];
+           download(response.id, Trainee.pdf, content)
+        })
+        .catch(error => console.log(error));
+}
